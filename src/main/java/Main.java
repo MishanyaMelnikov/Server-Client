85af148486c9b086139197f2e37b3ff3c9dfb6db
Mishanya.Melnikov <gallter@ya.ru>
2022-10-10 23:06:42 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        try (Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-        System.out.println("New connection accepted");
+            System.out.println("New connection accepted");
 
-        final String name = in.readLine();
+            final String name = in.readLine();
 
-        out.println(String.format("Привет, %s, твой порт %d", name, clientSocket.getPort()));
+            out.println(String.format("Привет, %s, твой порт %d", name, clientSocket.getPort()));
+        }
+
     }
 
 }
